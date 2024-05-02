604349b4c78aa94bc2ab5cba55ee2066c95e75a9
cemanuels <c.emanuel_ufc@outlook.com>
2024-05-01 22:10:21 -0400

    updating auth route and authenticated verify function

--- final_project/index.js
+++ final_project/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
+const authenticatedUser = require('./router/auth_users.js').authenticatedUser;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
@@ -11,7 +12,21 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const {username, password} = req.body
+
+    if (!username || !password) {
+        return res.status(404).json({ message: 'Username/password invalid!'})
+    }
+
+    if (authenticatedUser(username,password)) {
+        let accessToken = jwt.sign({
+          data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        req.session.authorization = {
+          accessToken,username
+      }
+      return res.status(200).send("User successfully logged in");
+    }
 });
  
 const PORT =5000;
